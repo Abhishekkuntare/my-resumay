51e9c4e5d53f0f420c2dbdc1551f20e2d50d8903
Abhishekkuntare <abhishekkuntare7@gmail.com>
2022-08-04 13:14:13 +0530

    after

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAt4-tyIPBnkJORlXxv_x8ZZcmwvL0LLqo',
-  authDomain: 'abhishekkuntareresumay.firebaseapp.com',
-  projectId: 'abhishekkuntareresumay',
-  storageBucket: 'abhishekkuntareresumay.appspot.com',
-  messagingSenderId: '392918823680',
-  appId: '1:392918823680:web:8f3fb1086940f0ff6779e5',
-  measurementId: 'G-H9RFSNCH3V'
-}
